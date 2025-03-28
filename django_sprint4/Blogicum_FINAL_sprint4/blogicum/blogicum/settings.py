327b1a7167b40ecc5dbb1b1ab159f6f37090d434
Baldislav <58264230+Baldislav@users.noreply.github.com>
2025-03-28 11:07:44 +0300

    Update settings.py

--- django_sprint4/Blogicum_FINAL_sprint4/blogicum/blogicum/settings.py
+++ django_sprint4/Blogicum_FINAL_sprint4/blogicum/blogicum/settings.py
@@ -8,9 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-towmz)2=!8fc=7is8b2&-o0mecab!+dkqbd53^t#)%_rjzybxq'
-)
+SECRET_KEY = 'django-insecure-_5bgk_a9x-rd9gtpvq(omxvx)g7x(!8ct^+a$)!_y75rg0jtcb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
